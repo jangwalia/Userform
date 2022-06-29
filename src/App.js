0dff3cd6cb7868dbea7d4658b5d17b96b6c41a38
jang walia <jangiwalia83@gmail.com>
2022-06-28 18:39:29 -0700

    add UserList in App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Adduser from "./components/users/Adduser";
 import './App.css';
-import Card from "./components/shared/Card";
+import { useState } from "react";
+import UsersList from "./components/users/UsersList";
+
 function App() {
+  const [Availableusers,setAvailableusers] = useState([])
+  const  handleSubmituser = (name,age)=>{
+    setAvailableusers((prevState)=>{
+      return [...prevState,{name,age,id : Math.random().toString()}]
+    })
+  }
   return (
     <div className="App">
-     <Adduser/>
+     <Adduser onAdduser={handleSubmituser}/>
+     <UsersList users = {Availableusers} />
     </div>
   );
 }
