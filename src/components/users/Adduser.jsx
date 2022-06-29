ea25274b8422a112e3ed945a0b0a35611206cfa1
jang walia <jangiwalia83@gmail.com>
2022-06-28 17:31:08 -0700

    use state to update input

--- src/components/users/Adduser.jsx
+++ src/components/users/Adduser.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Card from '../shared/Card';
 import Button from '../shared/Button';
 export default function Adduser() {
+  const [username,setUsername] = useState('');
+  const [age,setAge] = useState('');
+  const handleChangename = (e)=>{
+    setUsername(e.target.value);
+  }
+  const handleChangeage = (e)=>{
+    setAge(e.target.value);
+  }
   const handleSubmit = (e) =>{
     e.preventDefault();
+    console.log(username,age)
   }
   return (
     <Card>
       <form onSubmit={handleSubmit}>
         <label htmlFor="username">Username</label>
-        <input type="text" id = "username" />
+        <input type="text" id = "username" onChange = {handleChangename} />
         <label htmlFor="age">Age</label>
-        <input type="number" id = "age" />
+        <input type="number" id = "age" onChange = {handleChangeage} />
         <Button type = "submit">Add User</Button>
     </form>
     </Card>
